42262c6c3497a0b24794c14f75a4dcc5efd7d86b
cloudstrifed <86864294+cloudstrifed@users.noreply.github.com>
2023-06-10 12:05:50 -0500

    Delete hashcheckerlauncher.sh

--- hashcheckerlauncher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Enter full path to hashscan.py in both instances below
-# Save and run this with cron (sudo crontab -e) e.g. @reboot /home/<your-user-name>/<this-file.sh>
-
-python /PATH/TO/hashscan.py
-while [ $? -ne 0 ]; do
-    python /PATH/TO/hashscan.py
-done
